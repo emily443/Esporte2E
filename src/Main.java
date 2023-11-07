e5137f567c317ced8b3a7a1e3922891e2cd879ef
emily Carolini <emilycarolini008@gmail.com>
2023-11-07 07:36:37 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       Pessoa qualquer = new Pessoa();
+       qualquer.nome = "Raissa Leal"; // atributos
+       System.out.println(qualquer.nome); //saida de dados
+    }
+
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    //Atributos
+    private String nome;
+    private LocalDate dataNascimento;
+    private String cpf;
+    private double peso;
+    private double altura;
+}
